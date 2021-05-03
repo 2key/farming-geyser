3432890b6df2fe78e177290e39ce96504f43ae9f
madjarevicn <nikola.madjarevic@gmail.com>
2021-05-03 13:18:35 +0200

    Add LP and TwoKeyEconomy addresses and push staging-deploy.js migration

--- scripts/staging-deploy.js
+++ scripts/staging-deploy.js
@@ -7,13 +7,16 @@ async function main() {
 
   const contracts = getSavedContractAddresses()[hre.network.name];
 
+  // Distribution = 2M tokens, 100 days
+  // 6400 blocks daily  = 640000 blocks total
+  // 2M tokens over 640000 blocks = 3,125 tokens/block
   let currentBlock = await web3.eth.getBlockNumber();
   currentBlock += 100;
   console.log('startBlock: ' + currentBlock);
-  const rewardPerBlock = ethers.utils.parseEther("3.79"); //1 token per block
+  const rewardPerBlock = ethers.utils.parseEther("3.125"); //3,125 token per block
 
   const Farm = await hre.ethers.getContractFactory('Farm');
-  const farm = await Farm.deploy(contracts["HordToken"], rewardPerBlock, currentBlock);
+  const farm = await Farm.deploy(contracts["TwoKeyEconomy"], rewardPerBlock, currentBlock);
   await farm.deployed();
   console.log('Farm deployed with address: ', farm.address);
   saveContractAddress(hre.network.name, 'Farm', farm.address);
